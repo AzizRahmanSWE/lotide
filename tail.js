a5135487e20470f4b22d8f06cce032f46c58ea6d
Friies <bahram.parsa2019@gmail.com>
2023-02-01 16:23:57 -0500

    used eslint on tail and head.js

--- tail.js
+++ tail.js
@@ -8,8 +8,8 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
-  return array.slice(1)
-}
+  return array.slice(1);
+};
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
