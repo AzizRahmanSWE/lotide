1592bcd986d64227734c48672a34575901875075
Friies <bahram.parsa2019@gmail.com>
2023-02-07 17:54:59 -0500

    implement countLetters.js which counts letters from a sentence.

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+//The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
+
+//For example, countLetters('LHL') should return results indicating that L appears twice, and H once.
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(sentence) {
+  let result = {};
+  for (letter of sentence) {
+    if(letter.trim().length > 0) { //if no a space. checks if not a space.(.trim) removes whitespace from both ends of a string.
+      if(!result[letter]) //if result does not contains the letter.
+        result[letter] = 0 // initializes the letter
+      result[letter]++ //inciments by 1
+    }
+  }
+  return result
+}
+
+console.log(countLetters("lighthouse in the house"))
