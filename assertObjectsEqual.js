4e890a962c192d0ae918596c512ebb7f799639a8
Friies <bahram.parsa2019@gmail.com>
2023-03-20 21:12:43 -0400

    inspect utilily required

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,10 +1,11 @@
 const eqObjects = require("./eqObjects");
+const inspect = require('util').inspect; 
 
 const assertObjectsEqual = function(actual, expected) {
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
